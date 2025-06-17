7d7ef7bd3d7fe88489b58433d3f826b8bd5c715c
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-06-17 19:28:24 -0300

    added actions and other configuration

--- README.md
+++ README.md
@@ -1,37 +1,14 @@
-# 🚀 Getting started with Strapi
+# Setup DevOps A3
 
-Strapi comes with a full featured [Command Line Interface](https://docs.strapi.io/dev-docs/cli) (CLI) which lets you scaffold and manage your project in seconds.
+```bash
 
-### `pnpm` 
-
-Instal pnpm. [Learn more](https://pnpm.io/installation#using-npm) 
-
-```
-npm install -g pnpm@latest-10
-```
-
-### `develop`
-
-Start your Strapi application with autoReload enabled. [Learn more](https://docs.strapi.io/dev-docs/cli#strapi-develop)
+docker build -t devops-satc . -f ./Dockerfile
+docker run -p 1337:1337 devops-satc
 
 ```
-pnpm dev
-```
 
-### `start`
+## Authors
 
-Start your Strapi application with autoReload disabled. [Learn more](https://docs.strapi.io/dev-docs/cli#strapi-start)
-
-```
-pnpm start
-```
-
-### `build`
-
-Build your admin panel. [Learn more](https://docs.strapi.io/dev-docs/cli#strapi-build)
-
-```
-pnpm build
-```
----
-<sub>🤫 Psst! [Strapi is hiring](https://strapi.io/careers).</sub>
+- Eduardo Freitas
+- Sofia Martins
+- Higor Goulart
---
+++ src/scenarios/Category.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+const baseURL = `http://localhost:1337/api`;
+
+test.describe('Testes da API de Categoria', () => {
+    test('GET /categories - deve retornar uma lista de categorias', async ({ request }) => {
+        const response = await request.get(`${baseURL}/categories`);
+        expect(response.status()).toBe(200);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('data');
+        expect(Array.isArray(responseBody.data)).toBe(true);
+
+        responseBody.data.forEach((categoria: any) => {
+            expect(categoria.attributes).toHaveProperty('name');
+            expect(categoria.attributes).toHaveProperty('slug');
+            expect(categoria.attributes).toHaveProperty('description');
+        });
+    });
+
+    test('POST /categories - deve criar uma nova categoria', async ({ request }) => {
+        const novaCategoria = {
+            name: 'Categoria Exemplo',
+            slug: 'categoria-exemplo',
+            description: 'Descrição de exemplo',
+        };
+
+        const response = await request.post(`${baseURL}/categories`, {
+            data: { data: novaCategoria },
+        });
+        expect(response.status()).toBe(201);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('data');
+        expect(responseBody.data.attributes.name).toBe(novaCategoria.name);
+        expect(responseBody.data.attributes.slug).toBe(novaCategoria.slug);
+        expect(responseBody.data.attributes.description).toBe(novaCategoria.description);
+    });
+});
